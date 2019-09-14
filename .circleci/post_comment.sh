bf626397d2d3fa08cc1807a987b50fcd99f61c21
JoachimK <github@joachim.monoceres.uberspace.de>
2019-09-14 23:33:46 +0200

    Create post_comment.sh

---
+++ .circleci/post_comment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PR_API_URL=${echo -n ${CIRCLE_PULL_REQUEST} | sed sed -e 's_github.com_api.github.com/repos_g' -e 's/pull/issues/g' }
+
+
+echo "Posting to https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/issues/${CIRCLE_PR_NUMBER}/comments"
+curl --request POST \
+  --url "${PR_API_URL}/comments" \
+  --header 'accept: application/vnd.github.v3+json' \
+  --header 'content-type: application/json' \
+  -u JoachimK:${GITHUB_ACCESS_TOKEN} \
+  --data "{\"body\": \"Your preview for commit \`${CIRCLE_SHA1}\` can be found here: https://circleci.com/api/v1.1/project/github/test-wik/test-wik.github.io/${CIRCLE_BUILD_NUM}/artifacts/0/site/index.html \"}"
